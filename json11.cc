a7109772dcb0aa2155312fb84bbb88467e600211
Mark Essel <messel@gmail.com>
2014-07-16 19:10:51 -0400

    trying shenanigans to bypass non static initializers in gcc 4.7 and my local llvm g++

--- json11.cc
+++ json11.cc
@@ -212,13 +212,25 @@ public:
  * Static globals - static-init-safe
  */
 struct Statics {
-    const std::shared_ptr<JsonValue> null = make_shared<JsonNull>();
-    const std::shared_ptr<JsonValue> t = make_shared<JsonBoolean>(true);
-    const std::shared_ptr<JsonValue> f = make_shared<JsonBoolean>(false);
+    // const std::shared_ptr<JsonValue> null = make_shared<JsonNull>();
+    // const std::shared_ptr<JsonValue> t = make_shared<JsonBoolean>(true);
+    // const std::shared_ptr<JsonValue> f = make_shared<JsonBoolean>(false);
+    Statics();
+
+    const std::shared_ptr<JsonValue> null;
+    const std::shared_ptr<JsonValue> t;
+    const std::shared_ptr<JsonValue> f;
     const string empty_string;
     const vector<Json> empty_vector;
     const map<string, Json> empty_map;
 };
+
+Statics::Statics() : 
+    null(make_shared<JsonNull>()), 
+    t(make_shared<JsonBoolean>(true)), 
+    f(make_shared<JsonBoolean>(false))
+{
+}
 
 const Statics & statics() {
     static const Statics s {};
